39e3c6d8d5dae0880aea8b59dc69a7975958f186
kjpierc <kjpierc@clemson.edu>
2022-04-12 18:35:37 -0400

    login

--- login.php
+++ login.php
@@ -50,40 +50,6 @@ $link = mysqli_connect($hostname,$username,$pswd,$db_name) or die ('Could not co
             $username = $_POST['username']; // gathered from name 'username'
             $password = $_POST['password']; // gathered from name 'password'
 
-            //~
-            // // gather variable info from html form
-            // $check_username_query = "SELECT * from users WHERE username='{$username}'";
-            // $check_username_result = mysqli_query($link, $check_username_query) or die("Query error: ". mysqli_error($link)."\n");
-
-            // // loop through all results to see if it's empty
-            // $counter = 0;
-            // while ($line = mysqli_fetch_array($check_username_result, MYSQLI_ASSOC)){
-            //     // loop through each column
-            //     foreach($line as $col_value){	
-            //         if ($col_value == '' and $counter < 1) {
-            //             $error .= "<h3>username $username does not exist!</h3><br>";
-            //             $err_counter++;
-            //             $counter++;
-            //         }
-            //     }	
-            // }
-
-
-            //~
-            // print the count query
-            // $count_users_query = "SELECT COUNT(*) as cnt from users where username='{$username}'";
-            // $count_users_result = mysqli_query($link, $count_users_query) or die("Query error: ". mysqli_error($link)."\n");
-
-            // while ($row = $count_users_result->fetch_assoc()) {
-            //     $username_occurrences = $row['cnt'];
-            //     echo $row['cnt']."<br>";
-            // }
-
-            // if ($username_occurrences < 1) {
-            //     $error .= "<h3>username $username does not exist!</h3><br>";
-            //     $err_counter++;
-            // }
-
             // print the count query
             $count_users_query = "SELECT COUNT(*) as cnt from users where username='{$username}'";
             $count_users_result = mysqli_query($link, $count_users_query) or die("Query error: ". mysqli_error($link)."\n");
@@ -95,28 +61,11 @@ $link = mysqli_connect($hostname,$username,$pswd,$db_name) or die ('Could not co
 
             echo "number of users with the username $username = " . $count_users_result->num_rows . "<br>";
 
-            // // check if username and password match
-            // $check_password_query = "SELECT * from users WHERE username='{$username}' and password='{$password}'";
-            // $check_password_result = mysqli_query($link, $check_password_query) or die("Query error: ". mysqli_error($link)."\n");
-
-            // // loop through all results
-            // $counter = 0;
-            // while ($line = mysqli_fetch_array($check_password_result, MYSQLI_ASSOC)){
-            //     // loop through each column
-            //     foreach($line as $col_value){	
-            //         if ($col_value == '' and $counter < 1) {
-            //             $error .= "<h3>password is incorrect!</h3><br>";
-            //             $err_counter++;
-            //             $counter++;
-            //         }
-            //     }	
-            // }
-
             $matching_password_query = "SELECT * from users WHERE username='{$username}' and password='{$password}'";
             $matching_password_result = mysqli_query($link, $matching_password_query) or die("Query error: ". mysqli_error($link)."\n");
 
             if($matching_password_result->num_rows == 0) {
-                $error .= "<h3>username $username does not exist!</h3><br>";
+                $error .= "<h3>incorrect password</h3><br>";
                 $err_counter++;
             }
 
@@ -132,23 +81,12 @@ $link = mysqli_connect($hostname,$username,$pswd,$db_name) or die ('Could not co
             //~
             if (1 == 1) {
                 echo $error;
-                // ~
-                // echo "<h3>\$usernmae = " . $username . "</h3>";
-                // echo "<h3>\$password = " . $password . "</h3>";
                 echo "<h3>\$err_counter = " . $err_counter . "</h3>";
             }
 
             // otherwise, accept the login and add take the user to the home page
             else {
 
-                // construct db query
-                // $insert_query = "IF (0<=2) PRINT \'email address is valid\';";
-                // $insert_query = "IF ({$email} RLIKE {$valid_email_regex}) { echo 'email is valid!' };";
-                // $insert_query = "INSERT into users VALUES ('{$email}', '{$username}', '{$password}')";
-
-                // $result = mysqli_query($link, $insert_query) or die("Query error: ". mysqli_error($link)."\n");
-
-                // redirect them to the home page
                 //~THIS ISN'T WORKING
                 header('Location: http://webapp.computing.clemson.edu/~rjhay/MeTube/homepage.html');
 
