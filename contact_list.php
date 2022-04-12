2194a01dfe0295ca395dfa7891a7af0c4a47fc2f
Melissa Haueter <mhauete@clemson.edu>
2022-04-12 19:31:39 -0400

    basic contact list

--- contact_list.php
+++ contact_list.php
@@ -10,11 +10,38 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> The Contact List </title>
+        <title> Contact List </title>
         <link rel="stylesheet" type="text/css" href="homepage-style.css">
         <body>
             <h2> Contact List </h2>
 
+            <?php
+                // query the database or produce error message 
+                // add name, id number
+                $data_query = "SELECT username, email from users";
+                $result = mysqli_query($link, $data_query) or die("Query error: ". mysquli_error($link)."\n");;
+
+            ?>
+
+            <table class="table" width="50%" cellpadding="0" cellspacing="0">
+                <?php
+                    while($result_r = mysqli_fetch_row($result)) {
+                        $username = $result_r[0];
+                        $email = $result_r[1];
+                    
+                ?>
+                <tr valign="top">
+                    <td>
+                        <a href="#"> <?php echo $username;?> </a>
+                    </td>
+                    <td>
+                        <a> <?php echo $email;?> </a>
+                    </td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
         </body>
     </head>
 </html>
