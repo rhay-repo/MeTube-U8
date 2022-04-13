fbd3f3304b3a9f333fd5ba970e067e58245a0874
Melissa Haueter <mhauete@clemson.edu>
2022-04-13 16:34:40 -0400

    adjusting headers

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
-    require 'headers.php';
+    //require 'headers.php';
 
+    // verify that session started for page
     function session_start_check() {
         if ( php_sapi_name() !== 'cli' ) {
             if ( version_compare(phpversion(), '5.4.0', '>=') ) {
@@ -10,9 +11,21 @@
         return FALSE;
     }
 
+    // verify that user is logged in
     function user_login_check() {
         if(isset($_SESSION['logged_in'])) {
             return TRUE;
         } else { return FALSE; }
+    }
+
+    // add friend
+    function add_friend($uid, $uidf) {
+        $query = "SELECT * FROM friendList where uiD = $uid and uidF = $uidf";
+
+    }
+
+    // remove friend
+    function remove_friend() {
+
     }
 ?>
