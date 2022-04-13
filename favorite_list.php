2284d9c5faeda1131ca6bd726527652054739b4a
Melissa Haueter <mhauete@clemson.edu>
2022-04-13 15:37:29 -0400

    create favorite_list.php

---
+++ favorite_list.php
@@ -0,0 +1,62 @@
+<!-- WORK IN PROGRESS
+- NEED TO CREATE SQL TABLE WITH DATA TO FULLY IMPLEMENT
+- NEED TO CREATE "FAVORITE BUTTON" IN FUNCTIONS.PHP -->
+
+<?php
+    // include standard variables
+    require 'headers.php';
+    
+    // ~
+    // Connecting, selecting database
+    // $link = mysqli_connect($hostname,$username,$pswd,$db_name) or die ('Could not connect (ERROR):' .mysqli_error($link));
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Favorites List </title>
+        <link rel="stylesheet" type="text/css" href="homepage-style.css">
+        <body>
+            <h1> Favorites List </h1>
+
+            <?php
+                // query the database or produce error message 
+                $fav_query = "SELECT * from favoriteList";
+                $fav_result = mysqli_query($link, $fav_query) or die("Query error: ". mysquli_error($link)."\n");
+
+                $vid_query = "SELECT media_id, media_name from media";
+                $vid_result = mysqli_query($link, $vid_query) or die("Query error: ". mysqli_error($link). "\n");
+            ?>
+
+            <table class="table center" id="contacts" width="20%" cellpadding="0" cellspacing="0">
+                <tr>
+                    <th>Date Added</th>
+                    <th>Media Name</th>
+                    <th>Heart</th>
+                </tr>
+                <?php
+                    while($result_rfav = mysqli_fetch_row($fav_result) && $result_rvid = mysqli_fetch_row($vid_result)) {
+                        $date_added = $result_rfav[0];
+                        $media_name = $result_rvid[1];
+                    
+                ?>
+                <tr valign="top">
+                    <td>
+                        <a href="#"> <?php echo $date_added;?> </a>
+                    </td>
+                    <td>
+                        <a> <?php echo $media_name;?> </a>
+                    </td>
+                    <td>
+                        <button class="btn"> Favorite </button>
+                    </td>
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
+
+        </body>
+    </head>
+</html>
