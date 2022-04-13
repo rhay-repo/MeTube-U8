88d84bcb06792035685762057741e3b2dce1b246
kjpierc <kjpierc@clemson.edu>
2022-04-13 18:35:09 -0400

    guest

--- guest_homepage.php
+++ guest_homepage.php
@@ -9,7 +9,8 @@
         $_SESSION["loggedin"] = false;
         require 'headers.php';
         $_SESSION["loggedin"] = false;
-        echo "Please sign in or create an account";
+        echo '<div style="font-size:2em;color:white;text-align:center;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);border: 5px solid #FFFFFF;padding: 10px;"> Welcome to MeTube<br>Please sign in or Create an account' ;
+    
     ?>
    
 </body>
