86874d37071c69bb7b228d3f830eacfa8f0f8388
kjpierc <kjpierc@clemson.edu>
2022-04-13 18:41:39 -0400

    biew

--- guest_homepage.php
+++ guest_homepage.php
@@ -9,7 +9,8 @@
         $_SESSION["loggedin"] = false;
         require 'headers.php';
         $_SESSION["loggedin"] = false;
-        echo '<div style="font-size:2em;color:white;text-align:center;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);border: 5px solid #FFFFFF;padding: 10px;"> Welcome to MeTube<br>Please sign in or Create an account' ;
+        <link rel="stylesheet" type="text/css" href="register-style.css">
+            echo "<k1> Welcome to MeTube<br>Please sign in or Create an account </k1>";
     
     ?>
    
