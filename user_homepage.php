9414547af61e1c4010ce51e468910c367c740289
rhay-repo <reaganjhay@gmail.com>
2022-04-16 23:19:06 -0400

    fixed user_homepage.php

--- user_homepage.php
+++ user_homepage.php
@@ -8,10 +8,7 @@
     <?php 
         $_SESSION["loggedin"] = true; 
         require 'headers.php';
-<<<<<<< HEAD
-=======
 
->>>>>>> 65c334f13a05e22cf13b07c5021c58a982c96caf
         echo '<div style="font-size:2em;color:white;position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);border: 5px solid #FFFFFF;padding: 10px;"> Welcome, '. $_SESSION['username'] ;
     ?>
     <!-- <h4> 
