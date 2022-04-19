80d12cac084de69f9cb3a446dd7b55af8069a5a5
Melissa Haueter <mhauete@clemson.edu>
2022-04-19 19:33:52 -0400

    updates to channel

--- view_channel.php
+++ view_channel.php
@@ -1,5 +1,44 @@
 <!DOCTYPE html>
 <html>
+<style>
+		.content {
+			max-width: 500px;
+			margin: auto;
+		}
+
+		.btn-group button {
+			background-color: blueviolet; /* purple background */
+			border: 1px blueviolet; /* purple border */
+			color: white; /* White text */
+			padding: 10px 24px; /* Some padding */
+			cursor: pointer; /* Pointer/hand icon */
+			float: left; /* Float the buttons side by side */
+			max-width:500px;
+			margin: auto;
+		}
+
+		/* Clear floats (clearfix hack) */
+		.btn-group:after {
+			content: "";
+			clear: both;
+			display: table;
+		}
+
+		.btn-group button:not(:last-child) {
+			border-right: none; /* Prevent double borders */
+		}
+
+		/* Add a background color on hover */
+		.btn-group button:hover {
+			background-color: black;
+		}
+
+		h3 { 
+			text-align: center; 
+			color: white;
+		} 
+
+</style>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>View Profile</title>
@@ -17,11 +56,44 @@
     $user_email=$_SESSION['email'];
     $query=mysqli_query($con,"SELECT * FROM users where username='$id'")or die(mysqli_error());
     $row=mysqli_fetch_array($query);
+
+    function add_friend(&$uid, &$uidf) {
+		if($_SESSION['username'] != NULL) {
+			$query = "INSERT INTO contact_list VALUE ('{$uid}', '{$uidf}')";
+			$result = mysqli_query($_SESSION['link'], $query) or die("Query error test: ". mysqli_error($_SESSION['link'])."\n");;
+		} else {
+			$_SESSION['error'] = "You are not logged in, therefore you cannot complete this action!";
+
+			echo $_SESSION['error'];
+			unset($_SESSION['error']);
+		}
+    }
+
+	function subscribe(&$uid, &$uidf) {
+		if($_SESSION['username'] != NULL) {
+			$query = "INSERT INTO subscribe VALUE ('{$uid}', '{$uidf}')";
+			$result = mysqli_query($_SESSION['link'], $query) or die("Query error test: ". mysqli_error($_SESSION['link'])."\n");;
+		}
+		//  else {
+		// 	$_SESSION['error'] = "You are not logged in, therefore you cannot complete this action!";
+
+		// 	echo $_SESSION['error'];
+		// 	unset($_SESSION['error']);
+		// }
+    }
   ?>
-    <?php echo 
-        '<table class="table center" id="contacts" width="50%" cellpadding="1" cellspacing="1">
-            <br><h1>Your Profile</h1>    
-            <tr>
+    <?php 
+        
+        echo ' <br><h1>'.$id.' Profile </h1>';
+
+        echo "<div class='btn-group' style='width:100%'>
+                <button style='width=25%' onClick=".subscribe($_SESSION['username'], $user).">Subscribe</button>
+                <button style='width=25%' onClick=".add_friend($_SESSION['username'], $user).">Add Friend</button>
+            </div><br>";
+
+        echo'  <table class="table center" id="contacts" width="50%" cellpadding="1" cellspacing="1">';
+
+        echo'    <tr>
                 <th>Email</th>
                 <th>Username</th>
             </tr>
@@ -48,7 +120,7 @@
 
 <?php
     echo ' <table class="table center" id="contacts" width="10%" cellpadding="1" cellspacing="1">
-             <br><br><h1>Your Media</h1>
+             <br><br><h1>'.$id.' Media</h1>
                  <tr>
                     <th>Title</th>
                     <th>Date Published</th>
